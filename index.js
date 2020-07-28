f6f59f527fc2ded0c15d1ec0219ca905a8af6394
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2020-07-28 11:03:25 -0400

    Added forloop to add reviewers

--- index.js
+++ index.js
@@ -23,10 +23,12 @@ function run() {
     }
 
     const pullRequestNumber = context.payload.pull_request.number;
-    octokit.pulls.requestReviewers({
-      ...context.repo,
-      pull_number: pullRequestNumber,
-      reviewers: prReviewers,
+    prReviewers.forEach(reviewer => {
+      octokit.pulls.requestReviewers({
+        ...context.repo,
+        pull_number: pullRequestNumber,
+        reviewers: reviewer,
+      });
     });
   } catch (error) {
     core.setFailed(error.message);
